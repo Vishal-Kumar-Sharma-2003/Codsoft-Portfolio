56ce620842000d7ca4739fa70759cb06bc087167
Vishal Kumar Sharma <vishalsharma270503@gmail.com>
2025-07-29 16:27:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Typewrite effect for hero section
+const text = ["Vishal Kumar Sharma", "a Developer", "a Data Analyst"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+(function type(){
+    if(count === text.length){
+        count = 0;
+    }
+    currentText = text[count];
+    letter = currentText.slice(0,++index);
+
+    document.querySelector('.typewriter').textContent= letter;
+    if(letter.length === currentText.length){
+        count++;
+        index= 0;
+        setTimeout(type, 1000);
+    } else{
+        setTimeout(type, 100);
+    }    
+})();
